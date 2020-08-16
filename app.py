6ef71403171a730aa96027a16597fb91e021ee61
“glocalor <ranxgr@gmail.com>
2020-08-16 21:51:59 +0800

    add heroku deployment

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server # for Heroku deployment
 
 # assume you have a "long-form" data frame
 # see <a href="https://plotly.com/python/px-arguments/">https://plotly.com/python/px-arguments/</a> for more options
